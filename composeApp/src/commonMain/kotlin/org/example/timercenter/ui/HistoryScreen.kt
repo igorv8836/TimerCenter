0c65eac579fe1de56efe37c7240b88ed2a435e27
Maksim Vishnevskiy <maksvish135@yandex.ru>
2025-03-10 23:54:30 +0300

    feat: add HomeScreen, CreateScreen, and HistoryScreen files

---
+++ composeApp/src/commonMain/kotlin/org/example/timercenter/ui/HistoryScreen.kt
@@ -0,0 +1,16 @@
+package org.example.timercenter.ui
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HistoryScreen() {
+    Text("This is History Screen!")
+}
+
+@Preview
+@Composable
+fun HistoryScreenPreview() {
+    HistoryScreen()
+}
