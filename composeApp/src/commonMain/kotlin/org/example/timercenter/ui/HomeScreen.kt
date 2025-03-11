6e4c302b036666c04aa11e738a2446fe02bbaec1
Maksim Vishnevskiy <maksvish135@yandex.ru>
2025-03-11 22:46:00 +0300

    feat: refactor HomeScreen to support dynamic timer list.
    Updated HomeScreen to accept a list of TimerUiModel and display timers dynamically using LazyColumn. Introduced TimerUiModel data class and helper function to generate timer lists. Added placeholder Timer implementation and improved UI structure for customization.

--- composeApp/src/commonMain/kotlin/org/example/timercenter/ui/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/timercenter/ui/HomeScreen.kt
@@ -5,48 +5,63 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import kotlin.random.Random
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(timers: List<TimerUiModel>) {
     Column {
-        Text("All Timers(3)")
+        Text("All Timers")
         LazyColumn {
+            items(timers.size) { index ->
+                Timer(timer = timers[index],
+                    onStart = {},
+                    onPause = {},
+                    onReset = {})
+            }
 
         }
     }
 }
 
-@Composable
-fun Timer() {
-    Row {
-        Column {
-            Text(text = "Pomodoro Timer")
-            Text(text = "25 minutes")
-        }
-        Spacer(modifier = Modifier.fillMaxWidth(1f))
-        Button(
-            onClick = {},
-            colors = ButtonDefaults.buttonColors(
-                contentColor = Color(0xFF253646)
-            ),
-            shape = RoundedCornerShape(12.dp)
-        ) {
-            Text(text = "Start", color = Color.White)
-        }
+data class TimerUiModel(
+    val timerName: String,
+    val totalTime: Long = 60_000L
+)
+
+fun createTimerList(count: Int): List<TimerUiModel> {
+    return List(count) { index ->
+        TimerUiModel(
+            timerName = "Timer ${index + 1}",
+//            totalTime = Random.nextLong(30_000L, 300_000L) // от 30 секунд до 5 минут
+            totalTime = 300_000L // от 30 секунд до 5 минут
+        )
     }
 }
+
 
 @Preview
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen()
+    HomeScreen(createTimerList(50))
 }
