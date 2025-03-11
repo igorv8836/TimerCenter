cb903a573f5f8d547427c8fa4c2e11c757b10a29
Maksim Vishnevskiy <maksvish135@yandex.ru>
2025-03-12 00:41:23 +0300

    feat: refactor Screen model to enum and add route lookup by title
    Replaced the Screen data class with an enum to simplify screen representation. Added a companion object method to retrieve a Screen by its title, improving usability and code clarity. Removed unused imports and outdated screen model comments for a cleaner implementation.

--- composeApp/src/commonMain/kotlin/org/example/timercenter/ui/Screen.kt
+++ composeApp/src/commonMain/kotlin/org/example/timercenter/ui/Screen.kt
@@ -1,21 +1,14 @@
 package org.example.timercenter.ui
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.History
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.ui.graphics.vector.ImageVector
-import org.example.timercenter.navigation.CreateScreenRoute
-import org.example.timercenter.navigation.HistoryScreenRoute
-import org.example.timercenter.navigation.HomeScreenRoute
 
+enum class Screen(val route: String, val title: String) {
+    HOME("home", "Главная"),
+    CREATE("create", "Создать"),
+    HISTORY("history", "История");
 
-// 🔹 Модели экранов
-//sealed class Screen(val route: String, val title: String, val icon: ImageVector) {
-//    object Home : Screen(HomeScreenRoute.toString(), "Главная", Icons.Default.Home)
-//    object Create : Screen(CreateScreenRoute.toString(), "Создать", Icons.Default.Add)
-//    object History : Screen(HistoryScreenRoute.toString(), "История", Icons.Default.History)
-//}
-
-// 🔹 Экранная модель для BottomNavigationBar
-data class Screen(val route: String, val icon: ImageVector, val title: String)
+    companion object {
+        fun getRouteByTitle(title: String): Screen? {
+            return values().find { it.title == title }
+        }
+    }
+}
