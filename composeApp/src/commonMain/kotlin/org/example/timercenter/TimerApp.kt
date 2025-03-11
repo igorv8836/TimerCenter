d294ab8d8583890877b05ffd50e652b95b1cc7e6
Maksim Vishnevskiy <maksvish135@yandex.ru>
2025-03-11 21:39:14 +0300

    refactor: renamed Composable function App to TimerApp and updated all references

--- composeApp/src/commonMain/kotlin/org/example/timercenter/TimerApp.kt
+++ composeApp/src/commonMain/kotlin/org/example/timercenter/TimerApp.kt
@@ -1,11 +1,12 @@
 package org.example.timercenter
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,17 +16,26 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.timercenter.ui_common.AppTheme
 import org.example.timercenter.navigation.AppNavigation
 import org.example.timercenter.navigation.navigateToHome
-import org.jetbrains.compose.resources.painterResource
-import timercenter.composeapp.generated.resources.Res
-import timercenter.composeapp.generated.resources.compose_multiplatform
+import org.example.timercenter.ui.AppTopBar
+import org.example.timercenter.ui.BottomNavigationBar
 
 @Composable
-fun MyApp() {
+fun TimerApp() {
     AppTheme {
-        AppNavigation()
+        val navController = rememberNavController()
+        Scaffold(
+            topBar = { AppTopBar(navController) }, // Верхняя панель
+            bottomBar = { BottomNavigationBar(navController) } // Нижняя панель
+        ) { innerPadding ->
+            Box(modifier = Modifier.padding(innerPadding)) {
+                AppNavigation(navController)
+            }
+        }
+
     }
 }
 
