3623bea7c02dd81372c72aba9db01f735aba9d0c
Maksim Vishnevskiy <maksvish135@yandex.ru>
2025-03-13 12:36:22 +0300

    feat: add layout updates to HomeScreen
    Introduce height constraint to LazyColumn and add spacing with Spacer. Include a styled "Timer Groups" header with padding, font size, and color adjustments using MaterialTheme for better UI presentation.

--- composeApp/src/commonMain/kotlin/org/example/timercenter/ui/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/timercenter/ui/HomeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,6 +13,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -22,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,15 +34,27 @@ import kotlin.random.Random
 @Composable
 fun HomeScreen(timers: List<TimerUiModel>) {
     Column {
-        LazyColumn {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth()
+                .height(300.dp)
+        ) {
             items(timers.size) { index ->
                 Timer(timer = timers[index],
                     onStart = {},
                     onPause = {},
                     onReset = {})
             }
-
         }
+        Spacer(modifier = Modifier.height(18.dp))
+        Text(
+            text = "Timer Groups",
+            fontSize = 22.sp,
+            modifier = Modifier.padding(start = 12.dp),
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onBackground),
+        )
+
     }
 }
 
